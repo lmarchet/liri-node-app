2d074af875f8c8b8719318c4270eb4d733a763fe
lmarchet <leo.marchetti@hotmail.ca>
2018-06-19 01:05:59 -0400

    update

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+console.log('this is loaded');
+
+exports.twitter {
+    consumer_key: process.env.QR8dBWDvzRpHBA6ticeIo9wog,
+    consumer_secret: process.env.tDqa5A4w6hAMwJSjFmjHqQi2rUNXgBetgF0sxsiKALLAJ7JJKo,
+    access_token_key: process.env.	1008553029937876992-bZjG7d1kEDZazfwyAxsL41pX8VMj0H,
+    access_token_secret: process.env.Rju08t6nWcaiFpjaqNI4FkwmNdlbUF2oWEA4Yep8HYfGG
+};
+
+exports.spotify = {
+    id: 683f4102f62e4a69b611c0114798f147,
+    secret: b9a880035ce94d9abdeb2fb8f59adf66
+};
