eb09988071b688226f87363be70b5662961e5605
lmarchet <leo.marchetti@hotmail.ca>
2018-06-19 03:04:26 -0400

    Initial

---
+++ liri.js
@@ -0,0 +1,7 @@
+// read and set any environment variables with the dotenv package
+require("dotenv").config(); 
+
+//import the `keys.js` file and store it in a variable
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
