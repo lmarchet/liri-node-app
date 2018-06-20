ba6cb9ab2541ff675526fde4917975a4c2cbef97
lmarchet <leo.marchetti@hotmail.ca>
2018-06-20 00:50:54 -0400

    initial

--- liri.js
+++ liri.js
@@ -1,7 +1,22 @@
 // read and set any environment variables with the dotenv package
 require("dotenv").config(); 
 
-//import the `keys.js` file and store it in a variable
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var Twitter = require('twitter'); //mpm module to access twitter API
+var Spotify = require('spotify-web-api-node'); //npm module to access spotify API
+var Keys = require('./keys.js'); //keys.js file stores credentials to authentication
 
+// console.log(Keys.twitter);
+// console.log(Keys.spotify);
+
+//import the `keys.js` file and store it in a variable
+var spotify = new Spotify(Keys.spotify);
+var client = new Twitter(Keys.twitter);
+
+// Twiiter code from twitter npm
+var params = {screen_name: 'Leonard59288816', count: 20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    var twitterOutput = JSON.stringify(tweets, null, 4);
+    console.log(twitterOutput);
+  }
+});
